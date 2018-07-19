6b0bf055adc0d3b5444d70d32100450b365bddd5
wslippey <will.slippey@gmail.com>
2018-07-19 14:10:39 -0400

    Add more module stubs and test reqs

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(
+    version='0.1',
+    name='audio_bundler',
+    description='Bundle audiobook chapter files into one file',
+    packages=['audio_bundler']
+)
