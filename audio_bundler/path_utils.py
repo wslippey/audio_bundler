19095b5ca2acc23416bc7a7ee4bef5fef50cb6ba
wslippey <will.slippey@gmail.com>
2018-07-20 08:15:08 -0400

    Add pytest-datafiles

--- audio_bundler/path_utils.py
+++ audio_bundler/path_utils.py
@@ -1,2 +1,23 @@
+import re
+
 from pathlib import Path
 
+
+# Expected file naming convention for audio chapter files
+RE_AUDIO_CHAPTER = re.compile(
+    '^(?P<isbn>\d{13,16})_'
+    '(?P<abridged>(AB|DA))_'
+    '(?P<disc>\d{2})_'
+    '(?P<track>\d{3})_r'
+    '\.(?P<ext>\d+)$'
+)
+
+
+def check_in_out_paths(input_path, output_path='.'):
+    """
+    Verify input and optional output_paths are valid
+    :param input_path: Path to directory containing audio chapter files
+    :param output_path:
+    :return:
+    """
+    pass
